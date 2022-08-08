4f02e5a82f01e20dff6a302df1e21cf7495aef8d
Samson Mabetho <sammyhaward11@gmail.com>
2022-08-08 19:27:42 +0200

    rewritting code

---
+++ Jobi/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+
+
+
+def home(request):
+    return render(request, 'home.html')
